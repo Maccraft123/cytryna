134c592b2b5c5cb32bb17cc065d18c9ee6631331
Maccraft123 <maccraft123mc@gmail.com>
2023-11-02 21:53:12 +0100

    add more exheader functionality

--- cytryna/src/ncch.rs
+++ cytryna/src/ncch.rs
@@ -2,16 +2,14 @@ use std::mem;
 use std::os::raw::c_char;
 
 use crate::string::SizedCString;
+use crate::titleid::MaybeTitleId;
 use crate::{CytrynaError, Result};
 
+use bitfield::bitfield;
 use bitflags::bitflags;
 use derivative::Derivative;
 use static_assertions::assert_eq_size;
 
-// TODO:
-// - validate hashes
-// - validate sizes and offsets
-// - MediaSize<u32> if that shows up a lot
 #[derive(Derivative, Clone)]
 #[derivative(Debug)]
 #[repr(C)]
@@ -94,9 +92,6 @@ pub struct Ncch {
     data: [u8],
 }
 
-// TODO:
-// - modification of data inside of it?
-// - encryption and decryption
 impl Ncch {
     pub fn header(&self) -> &NcchHeader {
         &self.header
@@ -139,11 +134,205 @@ impl Ncch {
             Ok(mem::transmute(reg))
         }
     }
+    pub fn exheader(&self) -> Result<&Exheader> {
+        if self.header.exheader_size == 0 {
+            return Err(CytrynaError::MissingRegion);
+        }
+
+        unsafe {
+            Ok(mem::transmute(self.data[..self.header.exheader_size as usize].as_ptr()))
+        }
+    }
     pub fn romfs_region(&self) -> Result<&[u8]> {
         self.region(self.header.romfs_offset, self.header.romfs_size)
     }
     pub fn flags(&self) -> &NcchFlags {
         &self.header.flags
+    }
+}
+
+#[derive(Debug, Clone)]
+#[repr(C)]
+pub struct Exheader {
+    sci: SystemControlInfo,
+    aci: AccessControlInfo,
+    access_desc_sig: [u8; 0x100],
+    ncch_hdr_pubkey: [u8; 0x100],
+    aci_second: AccessControlInfo,
+}
+assert_eq_size!([u8; 0x800], Exheader);
+
+#[derive(Debug, Clone)]
+#[repr(C)]
+pub struct SystemControlInfo {
+    app_title: SizedCString<0x8>,
+    _reserved0: [u8; 0x5],
+    exheader_flags: ExheaderFlags,
+    remaster_version: u16,
+    text_code_set_info: CodeSetInfo,
+    stack_size: u32,
+    read_only_code_set_info: CodeSetInfo,
+    _reserved1: [u8; 0x4],
+    data_code_set_info: CodeSetInfo,
+    bss_size: u32,
+    dep_list: [MaybeTitleId; 0x30],
+    savedata_size: u64,
+    jump_id: u64,
+    _reserved2: [u8; 0x30],
+}
+assert_eq_size!([u8; 0x200], SystemControlInfo);
+
+bitflags! {
+    #[derive(Debug, Clone, Copy)]
+    pub struct ExheaderFlags: u8 {
+        const COMPRESS_EXEFS_CODE = 0x1;
+        const SD_APPLICATION = 0x2;
+        const _ = !0;
+    }
+}
+
+#[derive(Debug, Clone)]
+#[repr(C)]
+pub struct CodeSetInfo {
+    addr: u32,
+    phys_region_size_pages: u32,
+    size_bytes: u32,
+}
+assert_eq_size!([u8; 0xc], CodeSetInfo);
+
+#[derive(Debug, Clone)]
+#[repr(C)]
+pub struct AccessControlInfo {
+    arm11_syscaps: Arm11LocalSystemCaps,
+    arm11_kerncaps: Arm11KernelCaps,
+    arm9: Arm9AccessControl,
+}
+assert_eq_size!([u8; 0x200], AccessControlInfo);
+
+#[derive(Debug, Clone)]
+#[repr(C)]
+pub struct Arm11LocalSystemCaps {
+    program_id: MaybeTitleId,
+    core_version: u32,
+    n3ds_sysmode: New3dsSystemMode,
+    flag1: Flag1,
+    flag0: Flag0,
+    priority: u8,
+    resource_limit_desc: [u8; 0x20],
+    storage_info: StorageInfo,
+    service_access_control: [SizedCString<0x8>; 0x22],
+    _reserved0: [u8; 0xf],
+    resource_limit_category: ResourceLimitCategory,
+}
+assert_eq_size!([u8; 0x170], Arm11LocalSystemCaps);
+
+bitfield! {
+    #[derive(Clone, Copy)]
+    pub struct Flag0(u8);
+    impl Debug;
+
+    ideal_processor, _: 1, 0;
+    affinity_mask, _: 3, 2;
+    old3ds_system_mode, _: 7, 4;
+}
+
+bitfield! {
+    #[derive(Clone, Copy)]
+    pub struct Flag1(u8);
+    impl Debug;
+
+    enable_l2_cache, _: 0;
+    cpuspeed_804mhz, _: 1;
+}
+
+#[derive(Debug, Clone)]
+#[repr(u8)]
+pub enum New3dsSystemMode {
+    Legacy = 0,
+    Prod124Mb,
+    Dev1178Mb,
+    Dev2124Mb,
+}
+
+#[derive(Debug, Clone)]
+#[repr(C)]
+pub struct StorageInfo {
+    extdata_id: u64,
+    system_savedata_id: u64,
+    storage_access_unique_id: u64,
+    access_info: FsAccessInfo,
+}
+assert_eq_size!([u8; 0x20], StorageInfo);
+
+bitflags! {
+    #[derive(Debug, Clone, Copy)]
+    pub struct FsAccessInfo: u64 {
+        const CAT_SYSTEM_APPLICATION = 0x1;
+        const CAT_HARDWARE_CHECK = 0x2;
+        const CAT_FILESYSTEM_TOOL = 0x4;
+        const DEBUG = 0x8;
+        const TWL_CARD_BACKUP = 0x10;
+        const TWL_NAND_DATA = 0x20;
+        const BOSS = 0x40;
+        const SDMC = 0x80;
+        const CORE = 0x100;
+        const NANDRO_READONLY = 0x200;
+        const NANDRW = 0x400;
+        const NANDRO_WRITE_ACCESS = 0x800;
+        const CAT_SYSTEM_SETTINGS = 0x1000;
+        const CARDBOARD = 0x2000;
+        const EXPORT_IMPORT_IVS = 0x4000;
+        const SDMC_WRITEONLY = 0x8000;
+        const SWITCH_CLEANUP = 0x1_0000;
+        const SAVEDATA_MOVE = 0x2_0000;
+        const SHOP = 0x4_0000;
+        const SHELL = 0x8_0000;
+        const CAT_HOME_MENU = 0x10_0000;
+        const SEED_DB = 0x20_0000;
+        const _ = !0;
+    }
+}
+
+#[derive(Debug, Clone)]
+#[repr(u8)]
+pub enum ResourceLimitCategory {
+    Application = 0,
+    SysApplet = 1,
+    LibApplet = 2,
+    Other = 3,
+}
+
+#[derive(Debug, Clone)]
+#[repr(C)]
+pub struct Arm11KernelCaps {
+    descriptors: [u32; 0x1c],
+    _reserved0: [u8; 0x10],
+}
+assert_eq_size!([u8; 0x80], Arm11KernelCaps);
+
+#[derive(Debug, Clone)]
+#[repr(C)]
+pub struct Arm9AccessControl {
+    descriptors: Arm9Descriptors,
+    _pad: [u8; 0xd],
+    version: u8,
+}
+assert_eq_size!([u8; 0x10], Arm9AccessControl);
+
+bitflags! {
+    #[derive(Debug, Clone, Copy)]
+    // TODO: make sure this is correct
+    pub struct Arm9Descriptors: u16 {
+        const MOUNT_NAND = 0x1;
+        const MOUNT_NANRO_WRITE = 0x2;
+        const MOUNT_TWLN = 0x4;
+        const MOUNT_WNAND = 0x8;
+        const MOUNT_CARD_SPI = 0x10;
+        const USE_SDIF3 = 0x20;
+        const CREATE_SEED = 0x40;
+        const USE_CARD_SPI = 0x80;
+        const SD_APPLICATION = 0x100;
+        const MOUNT_SDMC_WRITE = 0x200;
     }
 }
 
